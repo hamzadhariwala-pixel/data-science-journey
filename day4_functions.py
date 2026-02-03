9893354fcf6199b59dd25b27723e49b2f4aa57af
hamzadhariwala-pixel <hamzadhariwala@gmail.com>
2026-02-03 11:52:01 +0530

    Create day4_functions.py

---
+++ day4_functions.py
@@ -0,0 +1,18 @@
+def greet(name):
+    print("Hello", name)
+
+greet("Hamza")
+greet("Data Scientist")
+
+def square(num):
+    print(num*num)
+square(59)
+
+def add(a, b):
+    print (a + b)
+    
+add(5602,3269)
+
+marks = [78, 85, 92, 67, 88, 35]
+x=sum(marks)/len(marks)
+print(x) 
